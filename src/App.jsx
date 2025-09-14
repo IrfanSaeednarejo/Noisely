1183796a6354f0336e817e811aa6d9765837f4b5
IrfanSaeednarejo <irfansaeed.narejo@gmail.com>
2025-09-14 10:05:02 +0500

    initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div> 
+       <h1>Hello g I am creating Noisely</h1>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
